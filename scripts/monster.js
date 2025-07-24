808d777cd9886945ada58d17365eca2bb5dc5785
Adam Jarvis <jarvisa@algonquincollege.com>
2025-07-24 12:32:14 -0400

    async functions part 2

--- scripts/monster.js
+++ scripts/monster.js
@@ -32,6 +32,57 @@ function createGallery(json){
 
     //use the Object library and a loop to iterate through the json object
     Object.values(json).forEach(monster => {
-        console.log(monster.large);
+        //use a template literal to build each monster image
+        //with information from the object
+        const imgTemplate = 
+            `<figure class="monster">
+                <img src="${monster.thumb}" data-large="${monster.large}" class="galimg">
+                <figcaption>${monster.species}</figcaption>
+            </figure>`;
+
+        //add the template to the images element
+        imgGal.innerHTML += imgTemplate;
     });
+}
+
+//add an event listener to the images section
+//and use event delegation to check if we clicked on one of the monster thumbnails
+document.querySelector(".images").addEventListener("click", function(event){
+    //event stores information about the click event
+    //so we can figure out which thing inside .images we clicked on
+    if(event.target.src){
+        //create an overlay
+        const overlay = document.createElement("div");
+        overlay.className = "overlay";
+
+        //build a template literal to put inside the overlay
+        const overTemplate = `<img src="${event.target.dataset.large}">`;
+
+        //add the template to the overlay
+        overlay.innerHTML = overTemplate;
+
+        //add an event listener to the overlay to destroy the overlay
+        //(a custom function we'll write)
+        overlay.addEventListener("click", destroyOverlay);
+
+        //add the overlay to the page
+        document.querySelector("body").appendChild(overlay);
+    }
+});
+
+//this function destroys the overlay
+function destroyOverlay(){
+    //ask the element's parent to destroy it
+    this.parentNode.removeChild(this);
+}
+
+//build filters using the list of traits requested from the API
+fetchFilters();
+
+async function fetchFilters(){
+    //send the query to the API and turn the data into json
+    const response = await fetch('https://qxplcd.ca/monster/monster.php?traits=list');
+    const json = await response.json();
+
+    console.log(json);
 }
